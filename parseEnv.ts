c198e99c7befa1343f1982ddc9af1dd95bf0858c
Keith Wade <keith@keithwade.com>
2024-10-28 13:23:18 -0400

    Add documentation

--- parseEnv.ts
+++ parseEnv.ts
@@ -7,14 +7,17 @@ import type {
 } from "https://deno.land/x/zod@v3.23.8/mod.ts";
 
 /**
- * This function validates the current environment's variables using the
- * provided Zod schema.
+ * This function parses the current environment's variables using the provided
+ * Zod schema.
  *
  * Environment variable permissions are requested individually so usage is
  * clear.
  *
  * Environment variable permissions can be refused without error for optional
  * values in the schema or when a default value is provided by the schema.
+ *
+ * @param schema The Zod schema to use to parse the environment variables.
+ * @returns Parsed environment variables.
  */
 export const parseEnv = <
   T extends ZodObject<ZodRawShape, UnknownKeysParam, ZodTypeAny>,
