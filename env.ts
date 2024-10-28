c87b0988c7ff9ebc98e077fa2b07dff18fa9c122
Keith Wade <keith@keithwade.com>
2024-10-28 09:00:35 -0400

    Initial implementation

---
+++ env.ts
@@ -0,0 +1,26 @@
+import { z } from "https://deno.land/x/zod@v3.23.8/mod.ts";
+import {
+  type UnknownKeysParam,
+  ZodFirstPartyTypeKind,
+  ZodObject,
+  type ZodRawShape,
+  type ZodTypeAny,
+} from "https://deno.land/x/zod@v3.23.8/types.ts";
+
+export const validateEnv = <
+  T extends ZodObject<ZodRawShape, UnknownKeysParam, ZodTypeAny>,
+>(
+  schema: T,
+): z.infer<typeof schema> => {
+  if (schema._def.typeName !== ZodFirstPartyTypeKind.ZodObject) {
+    throw new Error("Invalid schema. Must extend z.object.");
+  }
+
+  // Populate object to validate
+  const vars: Record<string, string | undefined> = {};
+  for (const key in schema._def.shape()) {
+    vars[key] = Deno.env.get(key);
+  }
+
+  return schema.parse(vars);
+};
---
+++ env_test.ts
@@ -0,0 +1,48 @@
+import { expect } from "jsr:@std/expect";
+import { afterAll, beforeAll, describe, it } from "jsr:@std/testing/bdd";
+import z from "https://deno.land/x/zod@v3.23.8/mod.ts";
+
+import { validateEnv } from "./env.ts";
+
+describe("validateEnv", () => {
+  const testEnv: Record<string, string> = {
+    TEST_FOO: "foo",
+    TEST_BAR: "42",
+    TEST_BAZ: "true",
+  };
+
+  beforeAll(() => {
+    for (const name in testEnv) {
+      Deno.env.set(name, testEnv[name]);
+    }
+  });
+
+  afterAll(() => {
+    for (const name in testEnv) {
+      Deno.env.delete(name);
+    }
+  });
+
+  it("should throw an error if schema does not extend z.object", () => {
+    expect.hasAssertions();
+
+    try {
+      validateEnv(z.string() as any);
+    } catch (err: any) {
+      expect(err).toBeInstanceOf(Error);
+      expect(err.message).toBe("Invalid schema. Must extend z.object.");
+    }
+  });
+
+  it("should handle empty schema", () => {
+    const actual = validateEnv(z.object({}));
+
+    expect(actual).toEqual({});
+  });
+
+  it("should only get requested env vars", () => {
+    const actual = validateEnv(z.object({ TEST_FOO: z.string() }));
+
+    expect(actual).toEqual({ TEST_FOO: "foo" });
+  });
+});
